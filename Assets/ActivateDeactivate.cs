1a78f9facc2d71b9e31d9d7f452249a0826f0aaf
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-19 14:42:25 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDeactivate : MonoBehaviour
+{
+    public GameObject myObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //if (myObject.activeInHierarchy)
+            //{
+            //    myObject.SetActive(false);
+            //}
+            //else
+            //{
+            //    myObject.SetActive(true);
+            //}
+
+            myObject.SetActive(!myObject.activeInHierarchy);
+            
+
+        }
+    }
+}
---
+++ Assets/TransformsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformsManager : MonoBehaviour
+{
+    public GameObject myCube;
+    public GameObject rotado;
+    public GameObject escalado;
+    public GameObject rotadoYEscalado;
+    public Vector3 originalRotation;
+    public Vector3 originalScale;
+    public bool isRotated;
+
+    void Start()
+    {
+        isRotated = false;
+        originalRotation = myCube.transform.eulerAngles;
+        originalScale = myCube.transform.localScale;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isRotated)
+            {
+                myCube.transform.eulerAngles = originalRotation;
+                isRotated = false;
+            }
+            else
+            {
+                myCube.transform.eulerAngles = rotado.transform.eulerAngles;
+                isRotated = true;
+            }            
+        }
+    }
+}
